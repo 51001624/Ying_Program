6b1f8219f1e13fe065dba86f9a53f8f735f26a93
Ying <leanhkiet@33-27.wlan.yzu.edu.tw>
2017-09-25 18:40:34 +0800

    Hello This is me

--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n This is project 1";
     return 0;
 }
 
