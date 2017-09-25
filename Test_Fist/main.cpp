eee2d86abb3dc1cabe4a89de44b236b2ebd19f44
Ying <leanhkiet@33-27.wlan.yzu.edu.tw>
2017-09-25 17:21:59 +0800

    Initial Commit

---
+++ Test_Fist/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Test_Fist
+//
+//  Created by Ying on 9/25/17.
+//  Copyright (c) 2017 Ying. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
